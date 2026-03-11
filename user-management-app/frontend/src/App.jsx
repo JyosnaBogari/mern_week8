6174001a4e930647f42053b7592b46c6b712fecb
Bogari Jyosna <jyosnabogari05@gmail.com>
2026-03-11 23:38:11 +0530

    App.jsx

--- user-management-app/frontend/src/App.jsx
+++ user-management-app/frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router"
 import RootLayout from "./components/RootLayout";
 import Home from "./components/Home";
 import AddUser from "./components/AddUser";
-import UserList from "./components/userList";
+import UserList from "./components/UserList";
 import User from "./components/User";
 
 function App() {
